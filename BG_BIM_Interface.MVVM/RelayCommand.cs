43bab4dd01c76faf2ce7521075701acc67adbc46
Max Wittich <maxwittich@gmx.de>
2021-05-05 13:30:00 +0200

    WIP

---
+++ BG_BIM_Interface.MVVM/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BG_BIM_Interface.MVVM
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _mAction { get; set; }
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action action)
+        {
+            _mAction = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._mAction();
+        }
+    }
+}
---
+++ BG_BIM_Interface.ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BG_BIM_Interface.MVVM;
+
+namespace BG_BIM_Interface.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public string MyText;
+        private int _counter;
+        public int Counter
+        {
+            get { return _counter; }
+            set
+            {
+                this._counter = value;
+                this.RaisePropertyChanged(nameof(this.Counter));
+            }
+        }
+        public ICommand CountUpCommand { get; set; }
+        public MainViewModel()
+        {
+            this.Counter = 0;
+            this.CountUpCommand = new RelayCommand(this.CountUp);
+        }
+        public void CountUp()
+        {
+            this.Counter++;
+        }
+    }
+}
