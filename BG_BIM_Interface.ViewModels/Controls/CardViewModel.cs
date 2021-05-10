8c52fe9d9e7ec97ba8183cd7236d3b9e681e1e2f
Max Wittich <maxwittich@gmx.de>
2021-05-10 17:57:57 +0200

    WIP

---
+++ BG_BIM_Interface.ViewModels/Controls/CardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BG_BIM_Interface.MVVM;
+
+namespace BG_BIM_Interface.ViewModels.Controls
+{
+    public class CardViewModel: BaseViewModel
+    {
+    }
+}
